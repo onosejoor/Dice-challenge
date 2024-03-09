99cf734ccca857c671bd48b9fa58bac1e33995d5
Onos Ejoor <156698969+onosejoor@users.noreply.github.com>
2024-03-09 10:57:06 +0100

    Add files via upload
    added a play button using what i learned about addEventListeners.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Player 1//
+document.querySelector(".play").addEventListener("click", function(){
 
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+    var randomNumber1 = Math.floor(Math.random()*6)+1;
 
 var randomImage1 = "dice" + randomNumber1 + ".png";
 
@@ -17,14 +18,16 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
 // Winner //
 
 if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "🚩Player 1 Wins"
+    document.querySelector("h1").textContent = "🚩Player 1 Wins!"
 }
 
 else if(randomNumber2 > randomNumber1){
-    document.querySelector("h1").textContent = "🚩Player 2 Wins"
+    document.querySelector("h1").textContent = "🚩Player 2 Wins!"
 }
 
 else{
-    document.querySelector("h1").textContent = "Draw"
+    document.querySelector("h1").textContent = "Draw!"
 
 }
+
+});
